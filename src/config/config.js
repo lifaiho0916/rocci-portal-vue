be5bac7d33bcb49678878c926fb5e34611e510c3
PhoenixDev1223 <phoenixdev1223@gmail.com>
2023-04-27 22:37:37 +0900

    Update

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   api:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:8082/api/v1/"
+      ? "http://localhost:5000/api/v1/"
       : "https://rocci-backend.herokuapp.com/api/v1/",
 };
